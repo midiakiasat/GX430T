d68f6504ff1e013f25df5f6d7347e9e4b7ec64c3
midiakiasat <kaaffilm@gmail.com>
2026-07-20 13:13:25 +0200

    Repair GX430T host launcher environment expansion

--- host-service/run-host.sh
+++ host-service/run-host.sh
@@ -16,19 +16,13 @@ if [[ -z "$PYTHON" ]]; then
   exit 69
 fi
 
-export GX430T_HOST_BIND="${
-  GX430T_HOST_BIND:-0.0.0.0
-}"
+export GX430T_HOST_BIND="${GX430T_HOST_BIND:-0.0.0.0}"
 
-export GX430T_HOST_PORT="${
-  GX430T_HOST_PORT:-43043
-}"
+export GX430T_HOST_PORT="${GX430T_HOST_PORT:-43043}"
 
 export GX430T_PORT="$GX430T_HOST_PORT"
 
-export GX430T_CLI="${
-  GX430T_CLI:-/usr/local/bin/gx430tctl
-}"
+export GX430T_CLI="${GX430T_CLI:-/usr/local/bin/gx430tctl}"
 
 exec "$PYTHON" \
   "$ROOT/host-service/gx430t_host.py" \
